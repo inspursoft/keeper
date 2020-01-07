456d8398d27e06cd1058391463e2cf783c638f57
wangkun_lc <wangkun_lc@inspur.com>
2020-01-07 17:16:53 +0800

    Update for release with tag.

--- keeper/integration.py
+++ keeper/integration.py
@@ -353,16 +353,9 @@ def tag_release():
 
   if checkout_sha is None:
     return "Bypass for none of checkout SHA or ref."
-  
+
   current_app.logger.debug("Version info: %s", version_info)
-  def request_release_url():
-    release_url = urljoin("http://localhost:5000", url_for("assistant.release", operator=username, release_repo=release_repo, release_branch=release_branch, category=checkout_sha, version_info=version_info))
-    current_app.logger.debug("Release URL: %s", release_url)
-    resp = requests.post(release_url)
-    return "Requested URL: %s with status code: %d" % (release_url, resp.status_code)
-  message = ""
-  try:
-    message = request_release_url()
-  except KeeperException as e:
-    message = "Failed to tag for release: %s" % (e,)
-  return message
+  release_url = urljoin("http://localhost:5000", url_for("assistant.release", operator=username, release_repo=release_repo, release_branch=release_branch, category=checkout_sha, version_info=version_info))
+  current_app.logger.debug("Release URL: %s", release_url)
+  resp = requests.post(release_url)
+  return "Requested URL: %s with status code: %d" % (release_url, resp.status_code)
