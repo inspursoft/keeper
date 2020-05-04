c5ebed625f67fda8de3952fd8719967f0203914e
王锟 <wangkun@wangkundeiMac.local>
2020-05-04 21:47:42 +0800

    Update for VM recycle when failure.

--- keeper/vm.py
+++ keeper/vm.py
@@ -15,7 +15,7 @@ def recycle_vm(current_app, vm_name, project_id, pipeline_id, status="N/A"):
   except KeeperException as e:
     current_app.logger.error(e.message)
     current_app.logger.error("VM: %s for project: %d does not exist, will clean up pipeline, runner info as failure...", vm_name, project_id)
-    KeeperManager.release_ip_runner_on_failure(project_id)
+    KeeperManager.release_ip_runner_on_failure(project_id, current_app)
   finally:
     KeeperManager.release_ip_runner_on_success(pipeline_id, status, current_app)
     KeeperManager.unregister_runner_by_name(vm_name, current_app)
