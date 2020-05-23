863cde308a59669db0dd4411460c16cf5c8f75aa
wangkun <wangkun_lc@inspur.com>
2020-05-24 01:29:53 +0800

    Update for create history folder.

--- keeper/assistant.py
+++ keeper/assistant.py
@@ -153,9 +153,9 @@ def release(action):
     actions.append({"action": action, "file_path": "install.sh", "content": KeeperManager.resolve_action_from_store(category, ".sh", current_app, project_name, version)})
     if action == "create":
       email = "%s@inspur.com" %(operator,)
-      history_file_path = "history/release-{}.md".format(version_info,)
-      KeeperManager.manipulate_file_to_repository(action, project_id, release_branch, operator, email, history_file_path, md_content, current_app)
-    KeeperManager.commit_files(project_id, release_branch, "Commit files about release: %s" %(version_info,), actions, current_app)
+      history_file_path = "history/release-{}.md".format(version,)
+      KeeperManager.manipulate_file_to_repository(action, project_id, version_info, operator, email, history_file_path, md_content, current_app)
+    KeeperManager.commit_files(project_id, version_info, "Commit files about release: %s" %(version_info,), actions, current_app)
     return "Successful released version: %s to project: %s with action: %s" %(version, project_name, action)
   except KeeperException as ke:
     message = "Failed to commit files to the repository: %s, with error: %s" % (release_repo, ke)
